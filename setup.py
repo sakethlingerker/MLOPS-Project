75c33a7fc6661cad21de267f7f5f81f26482147a
Saketh Lingerker <saketh1805@gmail.com>
2025-07-06 23:45:28 +0530

    Fresh clean commit with no secrets

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Saketh",
+    author_email="saketh1805@gmail.com",
+    packages=find_packages()
+)
